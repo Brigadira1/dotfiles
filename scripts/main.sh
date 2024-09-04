25deceef3f3c417c888341aad5f9715e1d08813d
brigadira1 <iavor.stoimenov@yahoo.com>
2024-09-04 19:46:49 +0300

    All scripts are reworked to work with stow

--- scripts/main.sh
+++ scripts/main.sh
@@ -13,7 +13,7 @@ NVIDIA_MADNESS=nvidia_madness.sh
 CURRENT_DIR=$(pwd)
 echo "Trying to source $ARCH_PACKAGES_SCRIPT, $CUSTOM_CONF_SCRIPT, $DOT_OS_CONFIGS and $NVIDIA_MADNESS"
 
-if [[ -f ./"${ARCH_PACKAGES_SCRIPT}"  &&  -x ./"${ARCH_PACKAGES_SCRIPT}"  && -f ./"${CUSTOM_CONF_SCRIPT}" && -x ./"${CUSTOM_CONF_SCRIPT}" && -f ./"${NVIDIA_MADNESS}" && -x ./"${NVIDIA_MADNESS}" ]]; then 
-    . ./"${ARCH_PACKAGES_SCRIPT}" && . ./"${CUSTOM_CONF_SCRIPT}" && . ./"${NVIDIA_MADNESS}"
+if [[ -f ./"${ARCH_PACKAGES_SCRIPT}"  &&  -x ./"${ARCH_PACKAGES_SCRIPT}" && -f ./"${STOW_SCRIPT}"  &&  -x ./"${STOW_SCRIPT}" && -f ./"${CUSTOM_CONF_SCRIPT}" && -x ./"${CUSTOM_CONF_SCRIPT}" && -f ./"${NVIDIA_MADNESS}" && -x ./"${NVIDIA_MADNESS}" ]]; then 
+    . ./"${ARCH_PACKAGES_SCRIPT}" && . ./"${STOW_SCRIPT}" && . ./"${CUSTOM_CONF_SCRIPT}" && . ./"${NVIDIA_MADNESS}"
 fi
 sudo shutdown -h now
