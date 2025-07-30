df6b8fcf3c21f8031d9b2e7b64864b4b125f3239
brigadira1 <iavor.stoimeonv@yahoo.com>
2025-07-30 17:57:04 +0300

    isort was removed from the neovim config

--- nvim/.config/nvim/lua/brigadira/plugins/formatting.lua
+++ nvim/.config/nvim/lua/brigadira/plugins/formatting.lua
@@ -12,7 +12,8 @@ return {
 				yaml = { "prettier" },
 				markdown = { "prettier" },
 				lua = { "stylua" },
-				python = { "isort", "black" },
+				-- python = { "isort", "black" },
+				python = { "ruff", "black" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
--- nvim/.config/nvim/lua/brigadira/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/brigadira/plugins/lsp/mason.lua
@@ -39,7 +39,7 @@ return {
 			ensure_installed = {
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
-				"isort", -- python formatter
+				-- "isort", -- python formatter
 				"black", -- python formatter
 				"ruff",
 				"eslint_d",
