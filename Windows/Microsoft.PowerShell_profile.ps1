2cdf9e1f02747d3b31770c46b0d111d2b432418e
Brigadira <iavor.stoimenov@yahoo.com>
2026-01-21 17:58:26 +0200

    A structure for Windows and all the window managers, file manages and bars was created for both home and work PCs

---
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+# ----------------------------------------------------------------
+# 1. Yazi Shell Wrapper (The "y" command)
+# ----------------------------------------------------------------
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi.exe $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
+# ----------------------------------------------------------------
+# 2. Zoxide Initialization
+# This ensures every 'cd' is recorded in the Zoxide database.
+# ----------------------------------------------------------------
+# NEW LINE
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+Invoke-Expression (&starship init powershell)
