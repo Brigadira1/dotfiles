ed4a1b0ac337195b4c7fab4fbbe60fd5ebf55d60
Brigadira <iavor.stoimenov@yahoo.com>
2026-01-25 00:49:33 +0200

    Changes to the power shell profile

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,22 @@
-# ----------------------------------------------------------------
-# 1. Yazi Shell Wrapper (The "y" command)
-# ----------------------------------------------------------------
-function y {
-    $tmp = (New-TemporaryFile).FullName
-    yazi.exe $args --cwd-file="$tmp"
-    $cwd = Get-Content -Path $tmp -Encoding UTF8
-    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
-        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
-    }
-    Remove-Item -Path $tmp
-}
-
-# ----------------------------------------------------------------
-# 2. Zoxide Initialization
-# This ensures every 'cd' is recorded in the Zoxide database.
-# ----------------------------------------------------------------
-# NEW LINE
-Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+# ----------------------------------------------------------------
+# 1. Yazi Shell Wrapper (The "y" command)
+# ----------------------------------------------------------------
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi.exe $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
+# ----------------------------------------------------------------
+# 2. Zoxide Initialization
+# This ensures every 'cd' is recorded in the Zoxide database.
+# ----------------------------------------------------------------
+# NEW LINE
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\iavor\.config\komorebi'
+$env:WHKD_CONFIG_HOME = 'C:\Users\iavor\.config\komorebi'
 Invoke-Expression (&starship init powershell)
