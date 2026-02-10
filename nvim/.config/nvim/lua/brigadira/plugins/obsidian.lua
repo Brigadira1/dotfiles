56176e5ae4b6b43ddfd78039136931252f89841e
Stoimenov <iavor.stoimenov@sap.com>
2026-02-10 12:02:20 +0200

    Mappings for valut switching were removed from the obsidian file and put inthe keymap file where they belong, removing the Warrning in neovim

--- nvim/.config/nvim/lua/brigadira/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/brigadira/plugins/obsidian.lua
@@ -57,21 +57,5 @@ return {
 			-- A map for custom variables, the key should be the variable and the value a function
 			substitutions = {},
 		},
-		mappings = {
-			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-			["<leader>zl"] = {
-				action = function()
-					return require("obsidian").util.gf_passthrough()
-				end,
-				opts = { noremap = false, expr = true, buffer = true },
-			},
-			-- Toggle check-boxes.
-			["<leader>zh"] = {
-				action = function()
-					return require("obsidian").util.toggle_checkbox()
-				end,
-				opts = { buffer = true },
-			},
-		},
 	},
 }
